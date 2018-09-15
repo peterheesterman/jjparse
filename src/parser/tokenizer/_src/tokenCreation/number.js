db3789656c8185c76fd23dc3c7558100106b884b
Peter Heesterman <heesterman.peter@gmail.com>
2018-09-15 21:31:13 +1200

    Remove the regex def type in favour of number

--- src/parser/tokenizer/_src/tokenCreation/number.js
+++ src/parser/tokenizer/_src/tokenCreation/number.js
@@ -1,15 +1,47 @@
 // @flow
 
-const { defTypeByRegex } = require('./defTypeByRegex')
+import type { Token } from '../../../types'
+import type { CharIterator } from '../charIterator'
+
+const { makeToken } = require('./makeToken')
+const { getValue } = require('../utils/selectors')
 
 const { number } = require('../../../tokenTypes')
 
-const regex = '-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?'
 const stopChars = [" ", "\t", "\n", "\r", "}", ":", ",", "]", "\""]
-const makeNumber = defTypeByRegex(number, regex, stopChars)
+
+const makeNumber = (stream: CharIterator): Token => {
+  const LOOK_LAST = true
+  const value = getValue(stream.next(LOOK_LAST))
+  const startPos = value.index
+  let endPos = startPos
+  
+  const chars = [value.char]
+  let steamEnd = false
+  while(!stopChars.includes(chars[chars.length - 1]) && !steamEnd) {
+    const next = stream.next()
+    if (!next.done) {
+      chars.push(getValue(next).char)
+      endPos++
+    } else {
+      steamEnd = true
+    }
+  }
+
+  if (!steamEnd) {
+    chars.splice(-1)
+    endPos--
+  }
+
+  const numberString = chars.join('')
+  if (isNaN(numberString)) {
+    throw new Error(`This value (${numberString}) is not a valid number`)
+  }
+
+  return makeToken(number, startPos, endPos, numberString)
+}
 
 module.exports = {
-  regex,
   make: makeNumber,
   type: number
 }
